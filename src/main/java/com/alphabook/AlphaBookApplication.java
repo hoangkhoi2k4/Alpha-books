ccd22623329e31a0732afc291f4dd7669f711698
hoangkhoi2k4 <lapyen30@gmail.com>
2026-04-05 02:18:24 +0700

    base project

---
+++ src/main/java/com/alphabook/AlphaBookApplication.java
@@ -0,0 +1,13 @@
+package com.alphabook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaBookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/alphabook/AlphaBookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alphabook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaBookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
